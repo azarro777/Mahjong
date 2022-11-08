2bd2448cd7ee2997ad1241aeebedc062e5f016e0
azarro <azarro777@gmail.com>
2022-11-08 15:12:25 +0200

    Fix add a check for prime numbers in the utils folder

--- src/utils/filledArray.js
+++ src/utils/filledArray.js
@@ -1,9 +1,24 @@
 
+const isSimple = (n) => {
+  if(n === 1 || n === 0) {
+    return false;
+  } else {
+    for (let i = 2; i < n; i++) {
+      if(n % i === 0) {
+        return false;
+      }
+    }
+    return n;
+  }
+}; 
+
 const unicArray = () => {
   let set = new Set();
   while(set.size < 16) {
     let num = Math.floor(Math.random() * (60 - 1 + 1) + 1);
-    set.add(num);
+    if (isSimple(num) !== false) {
+      set.add(num);
+    }
   }
   return [...set];
 };
