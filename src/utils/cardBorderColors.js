0bb8cc897570f4880e6f00c6ce5c19b66e2855ea
azarro <azarro777@gmail.com>
2022-11-14 12:11:45 +0200

    FIX create variables in component styles and create function with variables in utils to control the highlighting of the card during the game

---
+++ src/utils/cardBorderColors.js
@@ -0,0 +1,3 @@
+export const defaultCardColor = () => ({borderColor: "#CDCDCD"});
+export const matchedCardsColor = () => ({borderColor: "#0A0A0C"});
+export const compareCardsColor = () => ({borderColor: "#ED5517"})
