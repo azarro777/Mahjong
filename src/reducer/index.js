9aa2208cef68f77641f6abada1d0e8593aae7334
azarro <azarro777@gmail.com>
2022-11-21 09:26:30 +0200

    DELETE remove all .js files from the project

--- src/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import mahjongReducer from "./mahjongReducer";
-
-export const store = configureStore({
-	reducer: {
-		mahjong: mahjongReducer
-	}
-})
